49423e9db4ab6f0e643da0f7203cf7434ecd4298
Nick Chen <nchen.dev@mac.com>
2015-10-04 20:33:07 -0700

    Display map and searchbar
    Imports CoreLocation.frameworka and MapKit.framework.

---
+++ SimpleMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleMap
+//
+//  Created by Nick Chen on 10/4/15.
+//  Copyright © 2015 Nick Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
